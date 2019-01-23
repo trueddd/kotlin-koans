953bdd1df1f0eb331fbe2e608750659983e9593f
trueddd <kuzmichdima999@mail.ru>
2019-01-23 14:23:00 +0300

    collections done

--- src/ii_collections/n21Partition.kt
+++ src/ii_collections/n21Partition.kt
@@ -12,5 +12,6 @@ fun example8() {
 
 fun Shop.getCustomersWithMoreUndeliveredOrdersThanDelivered(): Set<Customer> {
     // Return customers who have more undelivered orders than delivered
-    todoCollectionTask()
+    val (moreDelivered, lessDelivered) = this.customers.partition { it.orders.count { it.isDelivered } >= it.orders.count { !it.isDelivered } }
+    return lessDelivered.toSet()
 }
